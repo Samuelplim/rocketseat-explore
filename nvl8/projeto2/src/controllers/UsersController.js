842d8a918fb6ddc7b1623cbf52d9625f44fb5383
Samuelplim <samuel12nerd@gmail.com>
2023-01-20 14:18:45 -0400

    Add bcryptjs

--- nvl8/projeto2/src/controllers/UsersController.js
+++ nvl8/projeto2/src/controllers/UsersController.js
@@ -1,12 +1,29 @@
+const { hash } = require("bcryptjs");
 const AppError = require("../utils/AppError");
+const sqliteConnection = require("../database/sqlite");
 
 class UsersController {
-  create(request, response) {
+  async create(request, response) {
     const { name, email, password, avatar } = request.body;
+    const database = await sqliteConnection();
+    const checkUserExists = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    );
     if (!name) {
       throw new AppError("O nome é obrigadorio!");
     }
-    response.status(201).json({ name, email, password, avatar });
+    if (checkUserExists) {
+      throw new AppError("Email já está cadastrado!");
+    }
+
+    const hashedPassword = await hash(password, 8);
+    await database.run(
+      "INSERT INTO users (name, email, password) VALUES (?,?,?)",
+      [name, email, hashedPassword]
+    );
+
+    response.status(201).json();
   }
 }
 module.exports = UsersController;
--- nvl8/projeto2/src/server.js
+++ nvl8/projeto2/src/server.js
@@ -1,14 +1,16 @@
 require("express-async-errors");
+const migrationsRun = require("./database/sqlite/migrations");
+const AppError = require("./utils/AppError");
 const express = require("express");
 const routes = require("./routes");
-const database = require("./database/sqlite");
-const AppError = require("./utils/AppError");
+
+migrationsRun();
 
 const app = express();
 app.use(express.json());
 
 app.use(routes);
-database();
+
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
     return response.status(error.statusCode).json({
