9566594d19cc38283aadcf712061b77038725ca8
samuelplim <samuel12nerd@gmail.com>
2023-07-22 12:51:41 -0400

    [feat] improvement responsibility

--- projeto final/frontend/src/components/InputLarge.jsx
+++ projeto final/frontend/src/components/InputLarge.jsx
@@ -1,9 +1,9 @@
 export const InputLarge = ({title, placeholder})=> {
 
   return  (
-  <div className="flex justify-center items-center gap-4">
-    <span>{title}</span>
-    <input placeholder={placeholder} type="text"name="aa"></input>
+  <div className="w-full flex flex-col items-start gap-2">
+    <span className="text-light-400">{title}</span>
+    <input placeholder={placeholder} type="text" className="w-full px-3.5 py-3 rounded-lg bg-dark-900"></input>
   </div>
   )
 }
--- projeto final/frontend/src/pages/SignInPage.jsx
+++ projeto final/frontend/src/pages/SignInPage.jsx
@@ -1,10 +1,17 @@
+import { ButtonLarge } from "../components/ButtonLarge"
 import { HeaderTitle } from "../components/HeaderTitle"
 import {InputLarge} from "../components/InputLarge"
+import { LinkLight } from "../components/LinkLight"
 
 export const SingInPage = ()=> {
   return(
-  <main className="bg-dark-400 flex h-screen justify-center">
+  <main className="bg-dark-400 px-10 flex flex-col h-screen justify-center items-center">
     <HeaderTitle/>
-    <InputLarge title="Email" placeholder="Exemplo: exemplo@exemplo.com.br"/>
+    <div className="flex w-full flex-col gap-8 mt-16">
+      <InputLarge title="Email" placeholder="Exemplo: exemplo@exemplo.com.br"/>
+      <InputLarge title="Senha" placeholder="No mínimo 6 caracteres"/>
+      <ButtonLarge title="Entrar"/>
+      <LinkLight title="Criar Uma conta"/>
+    </div>
   </main>
   )}
