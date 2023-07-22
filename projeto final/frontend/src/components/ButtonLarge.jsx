3c34b40eb10083e655cc36d3aeeb47a85cf14878
samuelplim <samuel12nerd@gmail.com>
2023-07-22 12:50:19 -0400

    [created] Button large

---
+++ projeto final/frontend/src/components/ButtonLarge.jsx
@@ -0,0 +1,11 @@
+export const ButtonLarge = ({title})=> {
+
+  return  (
+ 
+    <button className="w-full px-3.5 py-3 rounded-md text-sm bg-tints-tomato-100 hover:bg-tints-tomato-200 disabled:bg-tints-tomato-400">
+    {title}
+    </button>
+ 
+  )
+}
+
