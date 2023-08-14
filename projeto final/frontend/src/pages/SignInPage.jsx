fd7a83a4a82641c9249124e766405f52b51d43c7
samuelplim <samuel12nerd@gmail.com>
2023-08-14 19:23:36 -0400

    [feat] login

--- projeto final/frontend/src/pages/SignInPage.jsx
+++ projeto final/frontend/src/pages/SignInPage.jsx
@@ -1,14 +1,36 @@
-import { LinkLight,InputLarge, HeaderTitle, ButtonLarge } from "../components"
+import { useState } from "react";
 
-export const SingInPage = ()=> {
-  return(
-  <main className="bg-dark-400 px-10 flex flex-col h-screen justify-center items-center">
-    <HeaderTitle/>
-    <div className="flex w-full flex-col gap-8 mt-16">
-      <InputLarge title="Email" placeholder="Exemplo: exemplo@exemplo.com.br"/>
-      <InputLarge title="Senha" placeholder="No mínimo 6 caracteres"/>
-      <ButtonLarge title="Entrar"/>
-      <LinkLight title="Criar Uma conta" to="/register"/>
-    </div>
-  </main>
-  )}
+import { useAuth } from "../hooks/auth";
+
+import { LinkLight, InputLarge, HeaderTitle, ButtonLarge } from "../components";
+
+export const SingInPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { signIn } = useAuth();
+
+  const handleSignIn = () => {
+    signIn({ email, password });
+  };
+
+  return (
+    <main className="bg-dark-400 px-10 flex flex-col h-screen justify-center items-center">
+      <HeaderTitle />
+      <div className="flex w-full flex-col gap-8 mt-16">
+        <InputLarge
+          title="Email"
+          placeholder="Exemplo: exemplo@exemplo.com.br"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <InputLarge
+          title="Senha"
+          placeholder="No mínimo 6 caracteres"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <ButtonLarge title="Entrar" onClick={handleSignIn} />
+        <LinkLight title="Criar Uma conta" to="/register" />
+      </div>
+    </main>
+  );
+};
