a00a5ef3fefc9192f6e2dab69158f1e650c5e440
samuelplim <samuel12nerd@gmail.com>
2023-08-14 19:22:50 -0400

    [feat] rest props

--- projeto final/frontend/src/components/InputLarge.jsx
+++ projeto final/frontend/src/components/InputLarge.jsx
@@ -1,10 +1,13 @@
-export const InputLarge = ({title, placeholder})=> {
-
-  return  (
-  <div className="w-full flex flex-col items-start gap-2">
-    <span className="text-light-400">{title}</span>
-    <input placeholder={placeholder} type="text" className="w-full px-3.5 py-3 rounded-lg bg-dark-900"></input>
-  </div>
-  )
-}
-
+export const InputLarge = ({ title, placeholder, ...rest }) => {
+  return (
+    <div className="w-full flex flex-col items-start gap-2">
+      <span className="text-light-400">{title}</span>
+      <input
+        placeholder={placeholder}
+        type="text"
+        className="w-full px-3.5 py-3 rounded-lg bg-dark-900"
+        {...rest}
+      ></input>
+    </div>
+  );
+};
