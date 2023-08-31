e9ba567410c8951971cf076cd61a562e320ee8ca
samuelplim <samuel12nerd@gmail.com>
2023-08-30 20:04:32 -0400

    [feat] component card plate

--- projeto final/frontend/src/components/CardPlate.jsx
+++ projeto final/frontend/src/components/CardPlate.jsx
@@ -1,11 +1,61 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import {
+  HeartIcon,
+  ChevronRightIcon,
+  PlusIcon,
+  MinusIcon,
+} from "@heroicons/react/24/outline";
 
 export const CardPlate = ({ item }) => {
+  const [amount, setAmount] = useState(1);
+
+  const incrementAmount = () => {
+    if (amount === 10) return;
+    setAmount((prevAmount) => prevAmount + 1);
+  };
+
+  const decrementAmount = () => {
+    if (amount > 1) {
+      setAmount((prevAmount) => prevAmount - 1);
+    }
+  };
+  console.log("log", item);
   return (
-    <div>
-      <img src={() => require(item.uri)} alt="Foto da comida" />
-      <p>{item.title}</p>
-      <p>{item.price}</p>
+    <div className="relative p-6 w-52">
+      <HeartIcon className="absolute right-4 top-4 h-5 w-5" />
+      <div>
+        <div className="flex flex-col gap-3 mb-4">
+          {item.uri ? (
+            <img src={() => require(item.uri)} alt="Foto da comida" />
+          ) : (
+            <div className="h-20 w-20 m-auto flex items-center justify-center bg-slate-700 rounded-full">
+              <p className="">Sem foto</p>
+            </div>
+          )}
+          <div className="flex items-center justify-center">
+            <p>{item.name}</p> <ChevronRightIcon className="h-5 w-5" />
+          </div>
+
+          <p className="text-center font-roboto text-tints-cake-200">
+            {item.price}
+          </p>
+
+          <div className="flex justify-center gap-2">
+            <button onClick={incrementAmount}>
+              <PlusIcon className="h-5 w-5" />
+            </button>
+            <p>{amount}</p>
+            <button onClick={decrementAmount}>
+              <MinusIcon className="h-5 w-5" />
+            </button>
+          </div>
+        </div>
+
+        <button className="bg-tints-tomato-100 w-full rounded-md text-sm py-1">
+          incluir
+        </button>
+      </div>
     </div>
   );
 };
