d5941ffdb7b59711bbb5d15aa876c60ff1d444b5
samuelplim <samuel12nerd@gmail.com>
2023-09-02 00:05:46 -0400

    [refactor] html tags

--- projeto final/frontend/src/pages/HomePage.jsx
+++ projeto final/frontend/src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { ListHorizontal, NavMenu, CardPlate } from "../components";
+import { ListHorizontal, NavMenu, CardPlate, Footer } from "../components";
 import Bolachas from "../assets/imgs/bolachasDeFrutas.png";
 import { useEffect, useState } from "react";
 import { indexPlates } from "../services/plates.service";
@@ -15,25 +15,32 @@ export const HomePage = () => {
     fetchPlates();
   }, []);
   return (
-    <main className="bg-dark-400 flex flex-col h-screen justify-center items-center">
+    <>
       <NavMenu />
-      <div className="relative ml-4">
-        <div className="absolute  bottom-0 -left-16 opacity-80 overflow-hidden w-48 h-36">
-          <img src={Bolachas} alt="teste" className="w-full translate-y-1/1" />
+      <main className="bg-dark-400 flex flex-col h-screen justify-center items-center">
+        <div className="relative ml-4">
+          <div className="absolute  bottom-0 -left-16 opacity-80 overflow-hidden w-48 h-36">
+            <img
+              src={Bolachas}
+              alt="teste"
+              className="w-full translate-y-1/1"
+            />
+          </div>
+          <div className="w-[376px] h-[120px] rounded-sm px-5 pt-8 pb-4 pl-32 bg-gradient-to-t from-dark-600 to-dark-1000">
+            <p className="text-light-300 text-base font-sans font-semibold">
+              Sabores inigualáveis
+            </p>
+            <span className="text-xs text-light-300 ">
+              Sinta o cuidado do preparo com ingredientes selecionados.
+            </span>
+          </div>
         </div>
-        <div className="w-[376px] h-[120px] rounded-sm px-5 pt-8 pb-4 pl-32 bg-gradient-to-t from-dark-600 to-dark-1000">
-          <p className="text-light-300 text-base font-sans font-semibold">
-            Sabores inigualáveis
-          </p>
-          <span className="text-xs text-light-300 ">
-            Sinta o cuidado do preparo com ingredientes selecionados.
-          </span>
+        <div className="flex flex-col gap-6 mt-14">
+          <p>Refeições</p>
+          <ListHorizontal data={plates} />
         </div>
-      </div>
-      <div className="flex flex-col gap-6 mt-14">
-        <p>Refeições</p>
-        <ListHorizontal data={plates} />
-      </div>
-    </main>
+      </main>
+      <Footer />
+    </>
   );
 };
