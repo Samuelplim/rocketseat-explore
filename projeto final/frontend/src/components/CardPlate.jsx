85ab61205042ee9075b37c36229e0625258548b8
samuelplim <samuel12nerd@gmail.com>
2023-09-07 05:09:37 -0400

    [feat] to go EditPlate

--- projeto final/frontend/src/components/CardPlate.jsx
+++ projeto final/frontend/src/components/CardPlate.jsx
@@ -8,12 +8,16 @@ import {
 } from "@heroicons/react/24/outline";
 
 import { useAuth } from "../hooks/auth";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export const CardPlate = ({ item }) => {
   const [amount, setAmount] = useState(1);
   const { user } = useAuth();
+  const navigate = useNavigate();
 
+  const goEditPlate = (id) => {
+    navigate(`/plates/new/${id}`);
+  };
   const incrementAmount = () => {
     if (amount === 10) return;
     setAmount((prevAmount) => prevAmount + 1);
@@ -27,43 +31,43 @@ export const CardPlate = ({ item }) => {
   const linkedPlate = () => {};
 
   return (
-    <div className="relative p-6 w-52">
+    <div className="relative">
       {user.IsAdmin ? (
         <button className="absolute right-4 top-4" onClick={linkedPlate}>
           <HeartIcon className="h-5 w-5" />
         </button>
       ) : (
-        <Link className="absolute right-4 top-4" to={"/edit"}>
+        <button
+          className="absolute right-4 top-4"
+          onClick={() => goEditPlate(item.id)}
+        >
           <PencilSquareIcon className="h-5 w-5" />
-        </Link>
+        </button>
       )}
 
-      <div>
-        <div className="flex flex-col gap-3 mb-4">
-          {item.uri ? (
-            <img src={() => require(item.uri)} alt="Foto da comida" />
-          ) : (
-            <div className="h-20 w-20 m-auto flex items-center justify-center bg-slate-700 rounded-full">
-              <p className="">Sem foto</p>
-            </div>
-          )}
-          <div className="flex items-center justify-center">
-            <p>{item.name}</p> <ChevronRightIcon className="h-5 w-5" />
+      <div className="h-full w-52 p-6 flex flex-col gap-3 justify-between items-center">
+        {item.uri ? (
+          <img src={() => require(item.uri)} alt="Foto da comida" />
+        ) : (
+          <div className="h-20 w-20 m-auto flex justify-center items-center  bg-slate-700 rounded-full">
+            <p>Sem foto</p>
           </div>
+        )}
+        <div className="flex items-center gap-1">
+          <p className="text-sm">{item.name}</p>{" "}
+          <ChevronRightIcon className="h-3 w-3" />
+        </div>
 
-          <p className="text-center font-roboto text-tints-cake-200">
-            {item.price}
-          </p>
+        <p className="font-roboto text-tints-cake-200">R$ {item.price}</p>
 
-          <div className="flex justify-center gap-2">
-            <button onClick={incrementAmount}>
-              <PlusIcon className="h-5 w-5" />
-            </button>
-            <p>{amount}</p>
-            <button onClick={decrementAmount}>
-              <MinusIcon className="h-5 w-5" />
-            </button>
-          </div>
+        <div className="flex justify-center gap-2">
+          <button onClick={incrementAmount}>
+            <PlusIcon className="h-5 w-5" />
+          </button>
+          <p>{amount}</p>
+          <button onClick={decrementAmount}>
+            <MinusIcon className="h-5 w-5" />
+          </button>
         </div>
 
         <button className="bg-tints-tomato-100 w-full rounded-md text-sm py-1">
