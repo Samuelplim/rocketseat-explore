0e73a9424587c932fc526e7b9db7f318122b5e99
samuelplim <samuel12nerd@gmail.com>
2023-09-07 21:44:38 -0400

    [refactor] className to children

--- projeto final/frontend/src/components/ButtonLarge.jsx
+++ projeto final/frontend/src/components/ButtonLarge.jsx
@@ -1,8 +1,8 @@
-export const ButtonLarge = ({ title, ...rest }) => {
+export const ButtonLarge = ({ title, className, ...rest }) => {
   return (
     <button
       {...rest}
-      className="w-full px-3.5 py-3 rounded-md text-sm bg-tints-tomato-100 hover:bg-tints-tomato-200 disabled:bg-tints-tomato-400"
+      className={`w-full px-3.5 py-3 rounded-md text-sm ${className} hover:bg-tints-tomato-200 disabled:bg-tints-tomato-400`}
     >
       {title}
     </button>
